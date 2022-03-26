f2791efcaed07ccd46361310527862f0cea6f677
garg <saurav.garg@epfl.ch>
2022-03-26 19:38:11 +0530

    Remove files and add backtesting

---
+++ src/BackTesting.py
@@ -0,0 +1,16 @@
+import datetime
+import os
+import sys
+
+import backtrader as bt
+
+
+if __name__=="__main__":
+    cerebro = bt.Cerebro()
+    cerebro.broker.setcash(100000.0)
+
+    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    cerebro.run()
+
+    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
