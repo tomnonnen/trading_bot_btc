3086d5d1f9a8607465eb8090547f34719a29b200
tomnonnen <79456578+tomnonnen@users.noreply.github.com>
2022-09-22 14:10:38 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -4,3 +4,4 @@ LONG_SHORT_DATETIME="Date"
 BITCOIN_DATA_FILE="../data/data_btc.csv"
 BITCOIN_DATETIME="Date"
 MERGED_DATA_FILE="../data/merged_data.csv"
+API_SECRET= #add your secret coinglass API key
